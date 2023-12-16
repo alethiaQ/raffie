90a2a266d2c8227d3220694ad5eb1e9c059901b4
Alethia Quintero <alethiaq22@gmail.com>
2023-12-15 22:21:09 -0500

    add random selector action and button

--- raffie-frontend/src/components/RaffleJar.js
+++ raffie-frontend/src/components/RaffleJar.js
@@ -1,19 +1,30 @@
 import raffle from "../raffletickets.png";
 import IconButton from '@mui/material/IconButton';
-import { Refresh } from '@mui/icons-material';
+import { Refresh, CheckBox } from '@mui/icons-material';
 import { useDispatch } from 'react-redux';
-import {resetJar} from '../store/raffleJarSlice'
+import { pickEntry, resetJar } from '../store/raffleJarSlice'
+import { useSelector} from 'react-redux'
 function RaffleJar() {
     const dispatch = useDispatch();
+    const chosenEntry = useSelector(state => state.raffle.chosenEntry)
     function handleReset() {
         dispatch(resetJar());
+    }
+    function handleEntrySelection() {
+        dispatch(pickEntry())
     }
     return (
         <div>
             <IconButton size="small" onClick={event => handleReset()}>
                     <Refresh/>
-                </IconButton> 
+            </IconButton> 
+            <IconButton size="small" onClick={event => handleEntrySelection()}>
+                <CheckBox />
+            </IconButton>
             <img src={raffle} className="small-raffle-ticket" />
+            <p>
+            {chosenEntry}
+            </p> 
         </div>
     )
 }
--- raffie-frontend/src/store/raffleJarSlice.js
+++ raffie-frontend/src/store/raffleJarSlice.js
@@ -4,7 +4,8 @@ export const raffleJarSlice = createSlice({
   name: 'raffleJar',
   initialState: {
       entries: [],
-      max: 0
+      max: 0,
+      chosenEntry: ''
   },
     reducers: {
         addEntry: (state, action) => {
@@ -13,11 +14,16 @@ export const raffleJarSlice = createSlice({
         resetJar: state => {
             state.entries = [];
             state.max = 0;
+            state.chosenEntry = '';
+        },
+        pickEntry: state => {
+            const random = Math.floor(Math.random() * state.entries.length);
+            state.chosenEntry = state.entries[random]
         }
   }
 })
 
 // Action creators are generated for each case reducer function
-export const { addEntry, resetJar } = raffleJarSlice.actions
+export const { addEntry, resetJar, pickEntry } = raffleJarSlice.actions
 
 export default raffleJarSlice.reducer
